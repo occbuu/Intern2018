dfc672623926226edf9e097c1b89cf929cf26f33
JEREMY\JeremyPC <minhtrung391996@gmail.com>
2018-04-17 16:21:59 +0700

    Test.

--- MinhTrung/Matrix/src/main/java/com/example/demo/Controller.java
+++ MinhTrung/Matrix/src/main/java/com/example/demo/Controller.java
@@ -1,25 +1,20 @@
 package com.example.demo;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.context.request.*;
-import com.example.demo.Matrix;
 
 @RequestMapping(value = "/")
 public class Controller {
-
-	Matrix matrix = new Matrix(3, 3);
+	Matrix matrix;
 
 	@RequestMapping(value = "/")
 	public String home(@RequestParam(name = "matrix", required = false) int a[][], Model model) {
 		matrix.createRandomMatrix();
 		model.addAttribute("matrix", a);
 
-		model.addAttribute("col", matrix.getNoCols());
-		model.addAttribute("row", matrix.getNoRows());
+		model.addAttribute("col", matrix.getCols());
+		model.addAttribute("row", matrix.getRows());
 		return "home";
 	}
 
