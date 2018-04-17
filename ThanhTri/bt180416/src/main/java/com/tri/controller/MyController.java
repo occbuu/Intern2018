4f6062bc0bc86a7f5cbe7f8e1d4b4131dab561d7
Tri Nguyen <nguyenthtri22095@gmail.com>
2018-04-17 18:04:29 +0700

    Update class model Matrix add Bean.

--- ThanhTri/bt180416/src/main/java/com/tri/controller/MyController.java
+++ ThanhTri/bt180416/src/main/java/com/tri/controller/MyController.java
@@ -2,29 +2,32 @@ package com.tri.controller;
 
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.tri.model.*;
 import com.tri.req.*;
 import com.tri.res.*;
-import model.Matrix;
 
 @RestController
 @RequestMapping("/")
 public class MyController {
-
-	Matrix mat;
+	@Autowired
+	private Matrix mat;
 
 	@RequestMapping(value = "create", method = RequestMethod.POST)
 	public ResponseEntity<?> create(@RequestBody MyRequest body) {
 		SingleResult res = new SingleResult();
 		Random ran = new Random();
 		int c = body.get_c(), r = body.get_r();
-		mat = new Matrix(c, r, ran);
+		mat.setC(c);
+		mat.setR(r);
+		mat.getInit(ran);
+		// mat = new Matrix(c, r, ran);
 		res.setStatus("Success");
 		res.setResult(mat.getData());
 		return new ResponseEntity<>(res, HttpStatus.OK);
@@ -37,7 +40,7 @@ public class MyController {
 		String msg = "";
 		if (mat != null) {
 			msg = mat.save();
-		}else {
+		} else {
 			res.setStatus("Fail");
 			msg = "Please click create!";
 		}
@@ -51,12 +54,11 @@ public class MyController {
 	public ResponseEntity<?> read(@RequestBody MyRequest body) {
 		SingleResult res = new SingleResult();
 		res.setResult("Success");
-		if (mat == null) {
-			mat = new Matrix();
-		}
+//		if (mat == null) {
+//			mat = new Matrix();
+//		}
 		mat.setC(body.get_c());
 		mat.setR(body.get_r());
-		mat.setData(body.get_data() + "");
 		return new ResponseEntity<>(res, HttpStatus.OK);
 	}
 }
