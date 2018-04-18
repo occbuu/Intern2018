33a434385a94d2a3884cbfb57da3f6c84df8545c
ngiakhanh <ngiakhanh96@gmail.com>
2018-04-18 16:00:25 +0700

    Matrix.

---
+++ GiaKhanh/tmatrix/src/main/java/com/boraji/tutorial/springboot/controller/ReadMatrix.java
@@ -0,0 +1,41 @@
+package com.boraji.tutorial.springboot.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class ReadMatrix  {
+	private String path = "E:\\matrix.txt";
+	private FileInputStream fis;
+	public ReadMatrix(String path)
+	{
+		if (path != "")
+		{
+		this.path = path;
+		}
+	}
+	
+	public String Show() throws Exception {
+		File file = new File(path);
+		fis = new FileInputStream(file);
+	      char current;
+	      String temp = "";
+	      int i=0;
+	      while (fis.available() > 0) {
+	        current = (char) fis.read();
+	        if (i==0)
+	        {
+	        	temp += "<br>";
+	        }
+	        if (current == '\n')
+	        {
+	        	temp += "<br>";
+	        }
+	        else
+	        {
+	        	temp += Character.toString(current);
+	        }
+	        i++;
+	      }
+	     return temp;
+	}
+}
