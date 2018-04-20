1fae9b3f490302aeef26705d67b3d0fb46b3ce4f
ngiakhanh <ngiakhanh96@gmail.com>
2018-04-20 13:43:52 +0700

    Complete sync.

---
+++ GiaKhanh/tmatrix/src/main/java/com/matrix/springboot/controller/HelloController.java
@@ -0,0 +1,42 @@
+package com.matrix.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+   @RequestMapping("/")
+   public String index() {
+      return "index";
+   }
+
+   @PostMapping("/hello")
+   public String Add(@RequestParam("m") int m, @RequestParam("n") int n, @RequestParam("path") String path, @RequestParam("path_2") String path_2, Model model) throws Exception {
+      model.addAttribute("m", m);
+      model.addAttribute("n", n);
+      
+      if (path == "")
+      {
+    	  path = "E:\\matrix.txt";
+      }
+      
+      if (path_2 == "")
+      {
+    	  path_2 = "E:\\matrix.txt";
+      }  
+      
+      model.addAttribute("path", path);
+      model.addAttribute("path_2", path_2);
+      
+      MyMatrix k = new MyMatrix (m,n,path);
+      k.createAndSave();
+      ReadMatrix l = new ReadMatrix(path_2); 
+      model.addAttribute("show", l.Show());
+
+      return "hello";
+   }
+}
---
+++ GiaKhanh/tmatrix/src/main/java/com/matrix/springboot/controller/MyMatrix.java
@@ -0,0 +1,70 @@
+package com.matrix.springboot.controller;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Random;
+
+public class MyMatrix {
+	private int x=1, y=1, n;
+	private Random rand = new Random();
+	private String path = "E:\\matrix.txt";
+	
+	public MyMatrix (int x, int y, String path) {
+		this.x = x;
+		this.y = y;
+		if (path!="")
+		{
+		this.path = path;
+		}
+	}
+	public int getM()
+	{
+		return x;
+	}
+	
+	public void setM(int m)
+	{
+		x = m;
+	}
+	
+	public void setN(int n)
+	{
+		y = n;
+	}
+	
+	public int getN()
+	{
+		return y;
+	}
+	
+	public void setPath(String path)
+	{
+		this.path = path;
+	}
+	
+	public String getPath()
+	{
+		return path;
+	}
+	
+	public void createAndSave() throws IOException
+	{
+	File file = new File(path);
+	Writer output = new BufferedWriter(new FileWriter(file));
+	output.write(String.valueOf(x)+"x"+String.valueOf(y)+"\n");
+	for (int i=0;i<x;i++)
+	{	
+		for (int o=0;o<y;o++)
+		{
+			n = rand.nextInt(10);
+	        output.write(String.valueOf(n)+"\t");
+		}
+		output.write("\n");
+	}
+	output.close();
+	//System.out.println("Saved to "+path+" successfully!");
+	}
+}
